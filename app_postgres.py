c58d79656ec33849b35ca05cfd40d3b34e10e753
Benjamen Alford <benjamen.alford@gmail.com>
2021-05-12 12:52:47 -0500

    show getting data with api and postgres

---
+++ app_postgres.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+import os
+
+app = Flask(__name__)
+
+# your port is probably 5432 not 5433 like mine is here
+connection_string = "postgres:postgres@localhost:5433/customer_db"
+
+
+@app.route("/")
+def default():
+    return (os.environ.get('NAME', 'Name not configured'))
+
+
+@app.route("/api")
+def api():
+    engine = create_engine(f'postgresql://{connection_string}')
+    results = engine.execute("SELECT * FROM customer_name").fetchall()
+    data = []
+    index = 0
+    for item in results:
+        data.append({'id': results[index][0],
+                     'firstname': results[index][1],
+                     'lastname': results[index][2],
+                     })
+        index += 1
+    return jsonify(data)
+
+
+port = int(os.environ.get('PORT', 5000))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=port)
