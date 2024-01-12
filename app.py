49e3e12be448b06c3cd2fe85b7a48bea094ca2f4
Benjamen Alford <benjamen.alford@gmail.com>
2024-01-11 18:49:44 -0600

    lnn live example

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request,json,jsonify
+import pandas as pd
+from sklearn.cluster import KMeans
 
 app = Flask(__name__)
+
+#preload the model and train the data
+data_file_path = 'notebooks/pokemon.csv'
+data_df = pd.read_csv(data_file_path)
+data_df = data_df.loc[data_df['base_experience'] >0 ]
+filtered_df = data_df[data_df.describe().columns.to_numpy()]
+y = filtered_df['type_0_id']
+X = filtered_df[['stat_hp', 'stat_attack','stat_defense', 'stat_special-attack', 'stat_special-defense','stat_speed']]
+model= KMeans(n_clusters=5)
+model.fit(X)
+
 
 @app.route("/")
 def default():
@@ -29,6 +42,22 @@ def get_data():
         # do something with the data
         print(data)
         return data
+
+#Knn
+@app.route("/knn")
+def knn_route():
+    return render_template('knn.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.form['data']
+        # do something with the data
+        print(data)
+        #sample data [45,49,49,65,65,45]
+        prediction = model.predict([data])
+        print(prediction[0])
+        return f'{prediction[0]}'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5010)
